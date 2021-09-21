0a77de29d5dde3a17632c43c20e28589a068a4a9
Artsem.Audziyenka <Artsem.Audziyenka@itechart-group.com>
2021-09-21 12:14:07 +0300

    add: add base files to framework

---
+++ pages/home_page.py
@@ -0,0 +1,27 @@
+import allure
+from selenium.webdriver.common.keys import Keys
+from pages.base_page import BasePage
+
+
+class HomePage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    @allure.step("Expanding account menu")
+    def expand_account_menu(self):
+        self.driver.find_element(*LogInLocators.user_account_menu).click()
+
+    @allure.step("Opening login page")
+    def open_login_page(self):
+        self.driver.find_element(*LogInLocators.login_link).click()
+
+    @allure.step("Login with email: '1'")
+    def set_user_inputs(self, email, password):
+        self.driver.find_element(*LogInLocators.email_input).click()
+        self.driver.find_element(*LogInLocators.email_input).send_keys(email)
+        self.driver.find_element(*LogInLocators.password_input).click()
+        self.driver.find_element(*LogInLocators.password_input).send_keys(password, Keys.ENTER)
+
+    @allure.step("Logout")
+    def logout(self):
+        self.driver.find_element(*LogInLocators.logout_link).click()
